be850b44ed751e9f6d2acf0898dc586d17b7eded
Kacp3r3 <kacper.sawicki.it@gmail.com>
2022-03-15 11:25:04 +0100

    Basic project structure

---
+++ make_db.py
@@ -0,0 +1,12 @@
+from market import db
+from market.models import Item, User
+
+if __name__ == "__main__":
+    db.drop_all()
+    db.create_all()
+    user1 = User(username="Kacper", email="ex@ex.com", password_hash="###")
+    db.session.add(user1)
+    db.session.commit()
+    db.session.add(Item(name="Samsung S10e", price = 2500, barcode="123456789012", description="Nope", owner=user1.id ))
+    db.session.add(Item(name="Samsung S10", price = 3500, barcode="326426789012", description="Nope" ))
+    db.session.commit()
